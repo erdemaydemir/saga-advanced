425da2115db1591c192d20512b49492e05270876
Pova <povatr@gmail.com>
2022-12-09 19:33:09 +0300

    bug fix.

--- core/src/main/java/com/forguta/libs/saga/core/config/SagaAdvancedConfigurer.java
+++ core/src/main/java/com/forguta/libs/saga/core/config/SagaAdvancedConfigurer.java
@@ -5,6 +5,7 @@ import com.forguta.libs.saga.core.broker.rabbit.RabbitConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -34,11 +35,13 @@ public class SagaAdvancedConfigurer {
     }
 
     @Bean
+    @ConditionalOnBean(ApplicationProperties.class)
     public SagaMessageBrokerConfiguration sagaMessageBrokerConfiguration() {
         return new RabbitConfiguration(rabbitTemplate);
     }
 
     @Bean
+    @ConditionalOnBean(ApplicationProperties.class)
     public EventProcessorContextConfig eventProcessorContextConfig() {
         return new EventProcessorContextConfig(applicationContext);
     }
